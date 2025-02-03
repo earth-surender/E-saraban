c854ca9b2ab03b5005087a8a323b6acb55afbb99
earth-surender <140531433+earth-surender@users.noreply.github.com>
2025-02-03 17:02:13 +0700

    Add files via upload

---
+++ script/config.tailwind.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                blue: "#68CAD7", /* สีฟ้าของ menu */
+                secondary: "#EAB308", /* เหลือง */
+                customGray: "#6B7280", /* เทา */
+                softPink: "#F472B6" /* ชมพูอ่อน */
+            },
+            spacing: {
+                'w-ct-15' : '15%'
+            }
+        }
+    }
+};
